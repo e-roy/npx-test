fb53e7a5be21874a56f3693eeb6ecdf515a13713
e-roy <ericroupe@gmail.com>
2021-11-29 10:47:51 -0500

    update

--- bin/index.js
+++ bin/index.js
@@ -1,19 +1,10 @@
 #!/usr/bin/env node
-// const path = require("path");
-// const fs = require("fs");
-// const args = process.argv;
-
-// const root = path.resolve(args[2]);
-
-// if (!fs.existsSync(root)) {
-//   fs.mkdirSync(root);
-// }
-
 import { resolve } from "path";
 import { existsSync, mkdirSync } from "fs";
 const args = process.argv;
 
-const root = resolve(args[2]);
+// const root = resolve(args[2]);
+const root = resolve("somthing");
 
 if (!existsSync(root)) {
   mkdirSync(root);
