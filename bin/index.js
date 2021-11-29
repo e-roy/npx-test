d62b2a7afe123c0ca9aa848299db62c676ff4fa8
e-roy <ericroupe@gmail.com>
2021-11-29 11:29:23 -0500

    update

--- bin/index.js
+++ bin/index.js
@@ -9,5 +9,28 @@ if (!existsSync(root)) {
   mkdirSync(root);
 }
 
+import download from "download-git-repo";
+
+download("flippidippi/download-git-repo-fixture", "test/tmp", function (err) {
+  console.log(err ? "Error" : "Success");
+});
+
 // import got from "got";
 // import tar from "tar";
+// import { Stream } from "stream";
+// import { promisify } from "util";
+
+// const pipeline = promisify(Stream.pipeline);
+
+// function downloadAndExtractExample(){
+
+//   return pipeline(
+//     got.stream(
+//       `https://codeload.github.com/${username}/${name}/tar.gz/${branch}`
+//     ),
+//     tar.extract(
+//       { cwd: root, strip: filePath ? filePath.split("/").length + 1 : 1 },
+//       [`${name}-${branch}${filePath ? `/${filePath}` : ""}`]
+//     )
+//   );
+// }
