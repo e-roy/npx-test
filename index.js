27b29754440b0ad211e3d651c08421c148bc5080
e-roy <ericroupe@gmail.com>
2021-11-29 09:56:23 -0500

    intial test

---
+++ index.js
@@ -0,0 +1,13 @@
+let args = process.argv;
+
+const path = require("path");
+const fs = require("fs");
+
+const root = path.resolve(args[2]);
+
+if (!fs.existsSync(root)) {
+  fs.mkdirSync(root);
+}
+
+import got from "got";
+import tar from "tar";
