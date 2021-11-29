9d0568e3f8eb8406d386d20c939605984d5c2f25
e-roy <ericroupe@gmail.com>
2021-11-29 10:33:16 -0500

    update

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,22 @@
 #!/usr/bin/env node
-const path = require("path");
-const fs = require("fs");
+// const path = require("path");
+// const fs = require("fs");
+// const args = process.argv;
+
+// const root = path.resolve(args[2]);
+
+// if (!fs.existsSync(root)) {
+//   fs.mkdirSync(root);
+// }
+
+import { resolve } from "path";
+import { existsSync, mkdirSync } from "fs";
 const args = process.argv;
 
-const root = path.resolve(args[2]);
+const root = resolve(args[2]);
 
-if (!fs.existsSync(root)) {
-  fs.mkdirSync(root);
+if (!existsSync(root)) {
+  mkdirSync(root);
 }
 
 // import got from "got";
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-const path = require("path");
-const fs = require("fs");
+import { resolve } from "path";
+import { existsSync, mkdirSync } from "fs";
 const args = process.argv;
 
-const root = path.resolve(args[2]);
+const root = resolve(args[2]);
 
-if (!fs.existsSync(root)) {
-  fs.mkdirSync(root);
+if (!existsSync(root)) {
+  mkdirSync(root);
 }
 
 // import got from "got";
