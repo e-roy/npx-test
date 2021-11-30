b37e705f8e4046d9e2db7565e363368094f62266
e-roy <ericroupe@gmail.com>
2021-11-30 08:13:10 -0500

    update

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 #!/usr/bin/env node
-import path from "path";
+// import path from "path";
 import fs from "fs";
-import os from "os";
-import cpy from "cpy";
+// import os from "os";
+// import cpy from "cpy";
 
-const args = process.argv;
+// const args = process.argv;
 
 // const root = path.resolve(args[2]);
 const root = "something";
 
-const packageJson = {
-  name: "app-name",
-  private: true,
-  scripts: {
-    dev: "next dev",
-    build: "next build",
-    start: "next start",
-    lint: "next lint",
-  },
-};
+// const packageJson = {
+//   name: "app-name",
+//   private: true,
+//   scripts: {
+//     dev: "next dev",
+//     build: "next build",
+//     start: "next start",
+//     lint: "next lint",
+//   },
+// };
 
 const buildPackage = () => {
   if (!fs.existsSync(root)) {
@@ -33,28 +33,28 @@ const buildPackage = () => {
   // copyFiles();
 };
 
-const copyFiles = async () => {
-  await cpy("**", root, {
-    parents: true,
-    cwd: path.join(process.cwd(), "templates", "default"),
-    rename: (name) => {
-      switch (name) {
-        case "gitignore":
-        case "eslintrc.json": {
-          return ".".concat(name);
-        }
-        // README.md is ignored by webpack-asset-relocator-loader used by ncc:
-        // https://github.com/vercel/webpack-asset-relocator-loader/blob/e9308683d47ff507253e37c9bcbb99474603192b/src/asset-relocator.js#L227
-        case "README-template.md": {
-          return "README.md";
-        }
-        default: {
-          return name;
-        }
-      }
-    },
-  });
-};
+// const copyFiles = async () => {
+//   await cpy("**", root, {
+//     parents: true,
+//     cwd: path.join(process.cwd(), "templates", "default"),
+//     rename: (name) => {
+//       switch (name) {
+//         case "gitignore":
+//         case "eslintrc.json": {
+//           return ".".concat(name);
+//         }
+//         // README.md is ignored by webpack-asset-relocator-loader used by ncc:
+//         // https://github.com/vercel/webpack-asset-relocator-loader/blob/e9308683d47ff507253e37c9bcbb99474603192b/src/asset-relocator.js#L227
+//         case "README-template.md": {
+//           return "README.md";
+//         }
+//         default: {
+//           return name;
+//         }
+//       }
+//     },
+//   });
+// };
 
 function run() {
   // console.log("running");
